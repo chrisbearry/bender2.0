3566552a45b9afb8d2e1c710cdb00b35aab0bbaa
chrisbearry <88856854+chrisbearry@users.noreply.github.com>
2022-05-10 22:31:53 -0500

    Updated cmd with embeds and to show the question.

--- src/commands/Fun/8ball.js
+++ src/commands/Fun/8ball.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed, MessageAttachment } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -6,24 +7,55 @@ module.exports = {
 		.setDescription('Replies with an 8ball answer!!')
         .addStringOption(option => option.setName('question').setDescription('Your question').setRequired(true)),
 	async execute(interaction, client) {
+        const question = interaction.options.getString('question')
+        const user = interaction.user.username;
         let number = Math.floor(Math.random() * 6);
+        const embed1 = new MessageEmbed()
+                .setTitle(`${user} asked ${question}?`)
+                .setDescription("No, definitely not.")
+                .setColor("RANDOM")
+
+                const embed2 = new MessageEmbed()
+                .setTitle(`${user} asked ${question}?`)
+                .setDescription("Ask again later.")
+                .setColor("RANDOM")
+
+                const embed3 = new MessageEmbed()
+                .setTitle(`${user} asked ${question}?`)
+                .setDescription("It is uncertain.")
+                .setColor("RANDOM")
+                
+                const embed4 = new MessageEmbed()
+                .setTitle(`${user} asked ${question}?`)
+                .setDescription("Odds are not in your favor.")
+                .setColor("RANDOM")
+
+                const embed5 = new MessageEmbed()
+                .setTitle(`${user} asked ${question}?`)
+                .setDescription("Odds are in your favor.")
+                .setColor("RANDOM")
+
+                const embed6 = new MessageEmbed()
+                .setTitle(`${user} asked ${question}?`)
+                .setDescription("Yes, definitely so.")
+                .setColor("RANDOM")
         if (number == 0) {
-            return interaction.reply('Yes, definitely so.')
+            return interaction.reply({ embeds: [embed1] })
         }
         if (number == 1) {
-            return interaction.reply('No, definitely not.')
+            return interaction.reply({ embeds: [embed2] })
         }
         if (number == 2) {
-            return interaction.reply('Ask again later.')
+            return interaction.reply({ embeds: [embed3] })
         }
         if (number == 3) {
-            return interaction.reply('It is uncertain.')
+            return interaction.reply({ embeds: [embed4] })
         }
         if (number == 4) {
-            return interaction.reply('Odds are not in your favor.')
+            return interaction.reply({ embeds: [embed5] })
         }
         if (number == 5) {
-            return interaction.reply('Odds are in your favor.')
+            return interaction.reply({ embeds: [embed6] })
         }
 	},
 };
